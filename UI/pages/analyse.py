724c9b8193e773de8a06b28603e8bc51bba95ff5
zzsxd <nikitacapkov06@gmail.com>
2024-06-17 00:12:10 +0800

    add table on analyse page

--- UI/pages/analyse.py
+++ UI/pages/analyse.py
@@ -15,6 +15,7 @@ class Analyse:
     def __init__(self, db):
         super(Analyse, self).__init__()
         self.__crud = CRUD(db)
+        self.__table = None
 
     def analyse(self, pg: PageData):
 
@@ -25,6 +26,42 @@ class Analyse:
         def analyse_function(e):
             pg.navigator.navigate('analyse', pg.page)
 
+        ### LINECHART ###
+
+        # LineChart here
+
+
+        ### TABLE ###
+        def load_table_info(flag, restrictions=None):
+            if not flag:
+                data = self.__crud.get_basic_query()
+            else:
+                data = self.__crud.get_restricted_query(restrictions)
+            for row in data:
+                self.__table.rows.append(ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text(row[2])),
+                        ft.DataCell(ft.Text(row[6])),
+                        ft.DataCell(ft.Text(row[7])),
+                        ft.DataCell(ft.Text(value=row[8])),
+                    ],
+                ),
+                )
+
+
+        self.__table = ft.DataTable(
+            border=ft.border.all(2, "white"),
+            border_radius=10,
+            vertical_lines=ft.border.BorderSide(1, "black"),
+            horizontal_lines=ft.border.BorderSide(1, "black"),
+            columns=[
+                ft.DataColumn(ft.Text("Адрес")),
+                ft.DataColumn(ft.Text("Цена")),
+                ft.DataColumn(ft.Text("Опубликован")),
+                ft.DataColumn(ft.Text("Сайт")),
+            ]
+        )
+        load_table_info(False)
 
         ### BUTTONS ###
         parser_button = ft.FilledButton(text='Главная страница', width=170, height=32, on_click=mainpage)
@@ -35,7 +72,7 @@ class Analyse:
         parser_button = ft.FilledButton(text='Главная страница', width=170, height=32, on_click=mainpage)
         analys_button = ft.FilledButton(text='Анализ по агрегаторам', width=280, height=32, on_click=analyse_function)
 
-        pg.page.title = "Главная страница"
+        pg.page.title = "Анализ по агрегаторам"
         pg.page.bgcolor = "#828282"  # Установить белый цвет фона страницы
         pg.page.scroll = 'always'
 
@@ -53,4 +90,10 @@ class Analyse:
             ],
                 alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             ),
+            ft.Row([
+                self.__table,
+            ],
+                scroll="always",
+                alignment=ft.MainAxisAlignment.CENTER
+            )
         )
